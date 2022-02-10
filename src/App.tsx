2c16790ab156c78c395c1a9a3d2ee4f6748eb7df
Luke <lukehaparsons@gmail.com>
2022-02-10 16:26:16 +0000

    Add NLP review model

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,101 @@
-import * as React from 'react'
-
-import { ChakraProvider, Heading, Center, Input, Box, Button, Stack } from '@chakra-ui/react'
+import {
+  ChakraProvider,
+  Heading,
+  Center,
+  Input,
+  Box,
+  Button,
+  Stack,
+} from "@chakra-ui/react";
 import Message from "./Message";
+import { useState } from "react";
+
+
+const API_URL = "https://api-inference.huggingface.co/models/nlptown/bert-base-multilingual-uncased-sentiment";
+var HEADERS = new Headers();
+HEADERS.append("Authorization", "Bearer <HIDE API>");
+
+interface MessageObject {
+  contents: string;
+  user: string;
+  time: Date;
+}
 
 function App() {
   // 2. Use at the root of your app
+
+  const [msg, setMsg] = useState<string>("");
+  const [messages, setMessages] = useState<MessageObject[]>([]);
+
   return (
     <ChakraProvider>
       <Center>
-        <Stack spacing='20px'>
+        <Stack spacing="20px">
           <Box>
             <Heading>Chatbot</Heading>
           </Box>
-              <Box>
-                <Message></Message>
-                <Message></Message>
-                <Message></Message>
-              </Box>
-              <Box>
-                <Input placeholder='Enter Message'/>
-              </Box>
-              <Box>
-                <Button onClick={() => console.log("Hello World")}>Send Message</Button>
-              </Box>
+          <Box>
+            {messages.map((message, index) => (
+              <Message
+                key={index}
+                contents={message.contents}
+                user={message.user}
+                time={message.time}
+              />
+            ))}
+          </Box>
+          <Box>
+            <Input
+              placeholder="Enter Message"
+              value={msg}
+              onChange={(e) => setMsg(e.target.value)}
+            />
+          </Box>
+          <Box>
+            <Button
+              backgroundColor="lightblue"
+              ml={2}
+              onClick={async () => {
+                let newMessage: MessageObject = {
+                  contents: msg,
+                  user: "me",
+                  time: new Date(),
+                };
+
+                const  requestInit : RequestInit = {
+                  method: "POST",
+                  headers: HEADERS,
+                  body: JSON.stringify({inputs: msg}),
+                };
+                const response = await fetch(API_URL, requestInit);
+                const json = await response.json();
+                console.log(json);
+
+                const sortedRatings = json[0].sort(
+                  (val1: any, val2: any) => val2.score - val1.score
+                );
+
+                var reviewStr;
+
+                if(sortedRatings[0].score < 0.3) {
+                    reviewStr = "I'm not sure how many stars to give to this review - is it a proper review?";
+                } else if(sortedRatings[0].score > 0.3 && sortedRatings[0] < 0.6) {
+                    reviewStr = "I think this review is equivalent to " + sortedRatings[0].label + " out of 5, but I'm not too confident";
+                } else {
+                  reviewStr = "I think this review is equivalent to " + sortedRatings[0].label + " out of 5";
+                }
+
+                const aiMessage: MessageObject = {
+                  contents: reviewStr,
+                  user: "AI",
+                  time: new Date(),
+                };
+                setMessages([...messages, newMessage, aiMessage]);
+              }}
+            >
+              Send Message
+            </Button>
+          </Box>
         </Stack>
       </Center>
     </ChakraProvider>
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,11 +1,27 @@
 import { Box, Text } from "@chakra-ui/react";
 
-function Message() {
-return (
-    <Box backgroundColor="lightgray">
-            <Text color="black">Placeholder</Text>
+interface MessageProps {
+  contents: string;
+  user: string;
+  time: Date;
+}
+
+const Message: React.FC<MessageProps> = (props) => {
+  return (
+    <Box
+      alignSelf={props.user === "Me" ? "end" : "start"}
+      backgroundColor={props.user === "Me" ? "lightblue" : "lightgreen"}
+      fontWeight={600}
+    >
+      <Text color="black">{props.contents}</Text>
+      <Text color="black" size="sm" fontWeight="200">
+        {props.user}
+      </Text>
+      <Text color="black" size="sm" fontWeight="200">
+        {props.time.toDateString()} {props.time.toLocaleTimeString()}
+      </Text>
     </Box>
   );
-}
+};
 
 export default Message;
